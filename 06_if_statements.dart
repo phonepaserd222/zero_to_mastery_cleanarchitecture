1dbff5ff8e9db3cf3d27c67224f00141c399c44c
phonepaserd222 <phonepaserdsimphavong@gmail.com>
2024-01-18 13:52:37 +0700

    up

---
+++ 06_if_statements.dart
@@ -0,0 +1,11 @@
+void main() {
+  int age = 16;
+  print("user is $age years old.");
+  if (age >= 18) {
+    print("you can enter the movie.");
+  } else if (age == 16) {
+    print("you can enter the movie with your parents.");
+  } else {
+    print("you can not enter the movie.");
+  }
+}
---
+++ 07_swith_case.dart
@@ -0,0 +1,13 @@
+void main() {
+  final String name = "Tom";
+  switch (name) {
+    case "Hans":
+      print("my name is Hans");
+      break;
+    case "Peter":
+      print("my name is Peter!");
+      break;
+    default:
+      print("i don't know my name");
+  }
+}
