2a943e4d668c849642e85f435fe09790c75931c5
phonepaserd222 <phonepaserdsimphavong@gmail.com>
2024-01-24 14:32:39 +0700

    .

--- 08_while_loop.dart
+++ 08_while_loop.dart
@@ -1,7 +1,10 @@
 void main() {
   int number = 1;
+  int ku = 1;
   while (number < 100) {
     print("current $number");
-    number = number + 2;
+    number += 2;
   }
+  print("number after while: $number");
+  print(ku = ku + 2);
 }
