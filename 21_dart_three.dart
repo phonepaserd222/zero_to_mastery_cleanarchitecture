39380c83ea074b235b8ecd0e5d9d82690865a68c
phonepaserd222 <phonepaserdsimphavong@gmail.com>
2024-02-14 13:50:10 +0700

    .

--- 21_dart_three.dart
+++ 21_dart_three.dart
@@ -3,10 +3,34 @@ void main(List<String> args) {
   final (first, last) = records();
   print(first);
   print(last);
+
+  final describedDate = describeDate(DateTime.now());
+  print(describedDate);
+
+  final soundOfAnimal = whatDoesItSounds(Lion());
+  print(soundOfAnimal);
 }
+
+String describeDate(DateTime dt) => switch (dt.weekday) {
+      1 => "Feeling relaxed after the weekend?",
+      6 || 7 => "WEEKEND HURRAY!",
+      _ => "Hang in there",
+    };
 
 (String, String) records() {
   final firstName = "Max";
   final lastName = "steffen";
   return (firstName, lastName);
 }
+
+String whatDoesItSounds(Animal animal) => switch (animal) { Cow c => "$c moo", Sheep s => "$s say baa", Pig _ => "oink", _ => "unkwon" };
+
+sealed class Animal {}
+
+class Cow extends Animal {}
+
+class Sheep extends Animal {}
+
+class Pig extends Animal {}
+
+class Lion extends Animal {}
---
+++ 22_dart_null_safety.dart
@@ -0,0 +1,9 @@
+void main() {
+  int nonNullable = 10;
+  int? nullableInt = null;
+  nullableInt = 10;
+
+  late String description;
+  // print(description);
+  description = "description";
+}
