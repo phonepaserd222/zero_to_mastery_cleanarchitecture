a85bebd6c5df08b17d629396957af360f1d093ea
phonepaserd222 <68246096+phonepaserd222@users.noreply.github.com>
2024-02-28 16:59:18 +0700

    ,

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main (){
-runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -10,12 +11,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.green,
-        )
-      ),
+          appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.green,
+      )),
+      // open & close debug banner
+      // debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: ,),
+        appBar: AppBar(
+          title: const Text("flutter Basic"),
+        ),
+        body: Center(
+          child: Container(
+            height: 200,
+            decoration: const BoxDecoration(color: Colors.blue),
+            child: const Center(
+              child: Text(
+                "hello world!",
+                style: TextStyle(
+                  color: Colors.red, fontSize: 20, fontWeight: FontWeight.bold,
+                  // fontStyle: FontStyle.italic, houb bb font
+                ),
+              ),
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => debugPrint("clicked"),
+          child: const Icon(Icons.ac_unit),
+        ),
       ),
     );
   }
