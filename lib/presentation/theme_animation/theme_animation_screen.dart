41dbe24285a54fb0920ddd109ce83e38cb878c8e
phonepaserd222 <68246096+phonepaserd222@users.noreply.github.com>
2024-03-25 15:24:53 +0700

    end

--- lib/presentation/theme_animation/theme_animation_screen.dart
+++ lib/presentation/theme_animation/theme_animation_screen.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:zero_to_mastery_cleanarchitecture/application/theme_service.dart';
 import 'package:zero_to_mastery_cleanarchitecture/presentation/theme_animation/widgets/monn.dart';
